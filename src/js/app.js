74e5477b2fb6927d1f18522ef129918edb9bc076
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-03-14 18:13:33 -0400

    Got button to work, so now clicking the button will cause display number to increase.

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,16 @@
 let donutCount = 0;
-
 let result = document.getElementById('count');
 result.innerText = donutCount;
-    
+
+function AddDonutOnClick(){
+    let buttonClick = document.getElementsByTagName("button")[0];
+    buttonClick.addEventListener("click", IncreaseDonutCountByOne);
+}
+
+function IncreaseDonutCountByOne(){
+    donutCount++;
+    //console.log(donutCount);
+    result.innerText = donutCount;
+}
+
+

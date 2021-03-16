a279422c9a457c8614c4bfcecb49d1e550e13023
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-03-16 14:14:56 -0400

    Deleted commented out code that was no longer needed.

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,10 @@
-// let donutCount = 0;
-// let result = document.getElementById('count');
-// result.innerText = donutCount;
-
-// function AddDonutOnClick(){
-//     let buttonClick = document.getElementsByTagName("button")[0];
-//     buttonClick.addEventListener("click", IncreaseDonutCountByOne);
-// }
-
-// function IncreaseDonutCountByOne(){
-//     donutCount++;
-//     //console.log(donutCount);
-//     result.innerText = donutCount;
-// }
-
 const clicker = new DonutMaker();
 const clickDisplay = document.querySelector('.count');
 const mainClickButton = document.querySelector('.btn');
 
-//clickDisplay.innerText = clicker.getCount();
 const updateCount = (clickDisplay, clicker) => {
     clickDisplay.innerText = clicker.getCount();
 }
-
-// updateCount(clickDisplay, clicker);
-// mainClickButton.addEventListener('click', () => {
-//     clicker.addOne;
-//     updateCount(clickDisplay, clicker);
-// });
 
 const makeIncreaseButton = (mainClicker, clickDisp, clicker) => {
     mainClicker.addEventListener('click', () => {
